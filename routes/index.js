12262df4df379d49b69fdd9a4c07868d2989dd12
Tsaicming <tsai.cming@gmail.com>
2021-10-29 11:11:28 +0800

    feat: 建立 login、register 基本路由與 HTML 頁面 HTML 頁面引用 todo-list 的頁面

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,10 @@ const router = express.Router()
 // 總路由管理
 const home = require('./modules/home')
 const expenses = require('./modules/expenses')
+const users = require('./modules/users')
 
 router.use('/expenses', expenses)
+router.use('/users', users)
 router.use('/', home)
 
 
