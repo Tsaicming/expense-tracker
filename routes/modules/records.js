100256c7379bf4dab8ffed5bc2373882c5e7be1f
Tsaicming <tsai.cming@gmail.com>
2021-10-30 15:15:33 +0800

    feat: 新增修改支出功能

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -30,8 +30,37 @@ router.post('/', (req, res) => {
 
 // edit
 router.get('/:id/edit', (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+  Record.findOne({ _id, userId })
+    .lean()
+    .then(record => res.render('edit', { record }))
+    .catch(error => console.log('error'))
+})
 
-  res.render('edit')
+router.put('/:id', (req, res) => {
+  const userId = req.user._id
+  const { name, date, category, amount } = req.body
+  const _id = req.params.id
+  Category
+    .findOne({ name: category })
+    .lean()
+    .then(cate => {
+      const categoryId = cate._id
+      const icon = cate.icon
+      Record
+        .findOne({ _id, userId })
+        .then(record => {
+          record.name = name,
+            record.date = date,
+            record.amount = amount,
+            record.categoryId = categoryId,
+            record.icon = icon
+          return record.save()
+        })
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log('error'))
 })
 
 
